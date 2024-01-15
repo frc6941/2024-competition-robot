66e86a33158379deba78d063f6a12fafc3d8d673
Evan Luo <knacks.insects_0o@icloud.com>
2024-01-15 20:01:22 +0800

    fix: Replaced Front Right Swerve Module, Recalibrated CAN Coder

--- src/main/java/net/ironpulse/Constants.java
+++ src/main/java/net/ironpulse/Constants.java
@@ -60,6 +60,7 @@ public final class Constants {
 
         private static final double DRIVE_GEAR_RATIO = 6.7460317460317460317460317460317;
         private static final double STEER_GEAR_RATIO = 21.428571428571428571428571428571;
+
         // Estimated at first, then fudge-factored to make odom match record
         private static final Measure<Distance> wheelRadius = Inches.of(2.167);
 
@@ -101,37 +102,37 @@ public final class Constants {
         private static final int FRONT_LEFT_DRIVE_MOTOR_ID = 5;
         private static final int FRONT_LEFT_STEER_MOTOR_ID = 3;
         private static final int FRONT_LEFT_ENCODER_ID = 9;
-        private static final double FRONT_LEFT_ENCODER_OFFSET = -0.0537109375; // NEED WORKING
+        private static final double FRONT_LEFT_ENCODER_OFFSET = -0.0537109375; // TODO
 
-        private static final double FRONT_LEFT_X_POS_INCHES = 10.5; // NEED WORKING
-        private static final double FRONT_LEFT_Y_POS_INCHES = 10.5; // NEED WORKING
+        private static final double FRONT_LEFT_X_POS_INCHES = 10.5; // TODO
+        private static final double FRONT_LEFT_Y_POS_INCHES = 10.5; // TODO
 
         // Front Right
         private static final int FRONT_RIGHT_DRIVE_MOTOR_ID = 2;
         private static final int FRONT_RIGHT_STEER_MOTOR_ID = 7;
         private static final int FRONT_RIGHT_ENCODER_ID = 21;
-        private static final double FRONT_RIGHT_ENCODER_OFFSET = 0.09326171875; // NEED WORKING
+        private static final double FRONT_RIGHT_ENCODER_OFFSET = -0.46875;
 
-        private static final double FRONT_RIGHT_X_POS_INCHES = 10.5; // NEED WORKING
-        private static final double FRONT_RIGHT_Y_POS_INCHES = -10.5; // NEED WORKING
+        private static final double FRONT_RIGHT_X_POS_INCHES = 10.5; // TODO
+        private static final double FRONT_RIGHT_Y_POS_INCHES = -10.5; // TODO
 
         // Back Left
         private static final int BACK_LEFT_DRIVE_MOTOR_ID = 15;
         private static final int BACK_LEFT_STEER_MOTOR_ID = 14;
         private static final int BACK_LEFT_ENCODER_ID = 20;
-        private static final double BACK_LEFT_ENCODER_OFFSET = -0.312255859375; // NEED WORKING
+        private static final double BACK_LEFT_ENCODER_OFFSET = -0.312255859375;
 
-        private static final double BACK_LEFT_X_POS_INCHES = -10.5; // NEED WORKING
-        private static final double BACK_LEFT_Y_POS_INCHES = 10.5; // NEED WORKING
+        private static final double BACK_LEFT_X_POS_INCHES = -10.5; // TODO
+        private static final double BACK_LEFT_Y_POS_INCHES = 10.5; // TODO
 
         // Back Right
         private static final int BACK_RIGHT_DRIVE_MOTOR_ID = 10;
         private static final int BACK_RIGHT_STEER_MOTOR_ID = 6;
         private static final int BACK_RIGHT_ENCODER_ID = 12;
-        private static final double BACK_RIGHT_ENCODER_OFFSET = 0.0009765625; // NEED WORKING
+        private static final double BACK_RIGHT_ENCODER_OFFSET = 0.0009765625;
 
-        private static final double BACK_RIGHT_X_POS_INCHES = -10.5; // NEED WORKING
-        private static final double BACK_RIGHT_Y_POS_INCHES = -10.5; // NEED WORKING
+        private static final double BACK_RIGHT_X_POS_INCHES = -10.5; // TODO
+        private static final double BACK_RIGHT_Y_POS_INCHES = -10.5; // TODO
 
         private static final SwerveModuleConstants FrontLeft = ConstantCreator.createModuleConstants(
                 FRONT_LEFT_STEER_MOTOR_ID,
