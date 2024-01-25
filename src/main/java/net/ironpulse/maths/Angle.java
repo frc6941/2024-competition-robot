ea1cfa307f82ba3a19e55e17e8961bb0ed22ed3a
WilliamLi0623 <61577379+WilliamLi0623@users.noreply.github.com>
2024-01-25 16:13:04 +0800

    fix: Wheel radius wrong unit

--- src/main/java/net/ironpulse/maths/Angle.java
+++ src/main/java/net/ironpulse/maths/Angle.java
@@ -2,8 +2,6 @@ package net.ironpulse.maths;
 
 public class Angle {
     public static double continuousToPositive360(double angle) {
-        var _angle = angle % 360;
-        if (_angle < 0) _angle += 360;
-        return _angle;
+        return (angle+360)%360;
     }
 }
