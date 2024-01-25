464c56c488a1bd54d850258dc67bd9de88a498cd
WilliamLi0623 <61577379+WilliamLi0623@users.noreply.github.com>
2024-01-25 18:13:30 +0800

    feat: View CAN Coder Raw Data in Degrees in ShuffleBoard

--- src/main/java/net/ironpulse/subsystems/SwerveSubsystem.java
+++ src/main/java/net/ironpulse/subsystems/SwerveSubsystem.java
@@ -16,6 +16,7 @@ import edu.wpi.first.units.Time;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Subsystem;
@@ -43,6 +44,7 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
     @Override
     public void periodic() {
         updatePoseEstimatorFromLimelight();
+        showCANCoder();
     }
 
     /**
@@ -64,6 +66,13 @@ public class SwerveSubsystem extends SwerveDrivetrain implements Subsystem {
     public void zerotizePosition(){
             for (var module : Modules) {
             module.getCANcoder().setPosition(0);
+        }
+    }
+
+    private void showCANCoder(){
+        int i=0;
+        for (var module : Modules) {
+            SmartDashboard.putNumber("CANCoder " + i++, module.getCANcoder().getPosition().getValueAsDouble()*360%360);
         }
     }
 
