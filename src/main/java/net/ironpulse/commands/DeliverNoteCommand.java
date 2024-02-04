985ebdc46669d154285d91f2f636a39612eec22a
RealAllenDa <2153867950@qq.com>
2024-02-04 23:55:32 +0800

    feat(indicator): Finish indicator commands

--- src/main/java/net/ironpulse/commands/DeliverNoteCommand.java
+++ src/main/java/net/ironpulse/commands/DeliverNoteCommand.java
@@ -1,26 +1,43 @@
 package net.ironpulse.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import net.ironpulse.RobotContainer;
 import net.ironpulse.subsystems.beambreak.BeamBreakSubsystem;
 import net.ironpulse.subsystems.indexer.IndexerSubsystem;
+import net.ironpulse.subsystems.indicator.IndicatorIO;
 
+import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Volts;
 import static net.ironpulse.Constants.IndexerConstants.indexVoltage;
 
 public class DeliverNoteCommand extends Command {
     private final IndexerSubsystem indexerSubsystem;
     private final BeamBreakSubsystem beamBreakSubsystem;
+    private final RobotContainer robotContainer;
 
     public DeliverNoteCommand(
             IndexerSubsystem indexerSubsystem,
-            BeamBreakSubsystem beamBreakSubsystem
+            BeamBreakSubsystem beamBreakSubsystem,
+            RobotContainer robotContainer
     ) {
         this.indexerSubsystem = indexerSubsystem;
         this.beamBreakSubsystem = beamBreakSubsystem;
+        this.robotContainer = robotContainer;
     }
 
     @Override
     public void execute() {
         indexerSubsystem.getIo().setIndexVoltage(indexVoltage);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        indexerSubsystem.getIo().setIndexVoltage(Volts.of(0));
+        if (interrupted) return;
+        robotContainer.getIndicatorSubsystem()
+                .setPattern(IndicatorIO.Patterns.FINISH_SHOOT);
+        Commands.runOnce(() -> new RumbleCommand(robotContainer.getDriverController().getHID(), Seconds.of(0.5)));
     }
 
     @Override
--- src/main/java/net/ironpulse/commands/PreShootCommand.java
+++ src/main/java/net/ironpulse/commands/PreShootCommand.java
@@ -1,19 +1,40 @@
 package net.ironpulse.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import net.ironpulse.Constants;
+import net.ironpulse.RobotContainer;
+import net.ironpulse.subsystems.indicator.IndicatorIO;
 import net.ironpulse.subsystems.shooter.ShooterSubsystem;
 
 import static net.ironpulse.Constants.ShooterConstants.shootVoltage;
 
 public class PreShootCommand extends Command {
     private final ShooterSubsystem shooterSubsystem;
+    private final RobotContainer robotContainer;
 
-    public PreShootCommand(ShooterSubsystem shooterSubsystem) {
+
+    public PreShootCommand(ShooterSubsystem shooterSubsystem,
+                           RobotContainer robotContainer) {
         this.shooterSubsystem = shooterSubsystem;
+        this.robotContainer = robotContainer;
+    }
+
+    @Override
+    public void initialize() {
+        robotContainer.getIndicatorSubsystem()
+                .setPattern(IndicatorIO.Patterns.SHOOTING);
     }
 
     @Override
     public void execute() {
         shooterSubsystem.getIo().setShooterVoltage(shootVoltage);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.getIo()
+                .setShooterVoltage(Constants.ShooterConstants.shooterConstantVoltage);
+        if (!interrupted) return;
+        robotContainer.getIndicatorSubsystem().resetToLastPattern();
     }
 }
